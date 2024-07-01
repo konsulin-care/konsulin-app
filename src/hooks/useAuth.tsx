127b31d82a85cbe689922cd3dcc7ebcc6468d56b
hariadiarief <adi.arief77@gmail.com>
2024-07-01 17:26:18 +0700

    add: loader

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,23 +1,36 @@
 import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-import { useEffect } from 'react'
+interface WithAuthProps {
+  userRole?: 'patient' | 'clinician' | 'guest'
+}
 
 const withAuth = (
-  WrappedComponent: React.ComponentType<any>,
+  WrappedComponent: React.ComponentType<WithAuthProps>,
   allowedRoles: string[] = []
 ) => {
   const Wrapper = (props: any) => {
+    const [isVerified, setIsVerified] = useState(false)
+    const [userRole, setUserRole] = useState<string | null>(null)
+
     const router = useRouter()
     const pathname = usePathname()
 
-    const token = localStorage.getItem('token')
-    const userRole = localStorage.getItem('userRole')
-
     useEffect(() => {
-      if (!token || !userRole || !allowedRoles.includes(userRole)) {
+      const token = localStorage.getItem('token')
+      const role = localStorage.getItem('userRole')
+
+      if (!token || !role || !allowedRoles.includes(role)) {
         router.push(`/login?redirect=${pathname}`)
+      } else {
+        setUserRole(role)
+        setIsVerified(true)
       }
-    }, [])
+    }, [allowedRoles, pathname, router])
+
+    if (!isVerified) {
+      return <div>Loading...</div>
+    }
 
     return <WrappedComponent {...props} userRole={userRole} />
   }
