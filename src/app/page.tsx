0c8f2304f8d91a8a823dbc52c8a742e040f3b7a5
hariadiarief <adi.arief77@gmail.com>
2024-07-04 23:08:43 +0700

    slicing home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/header'
 import NavigationBar from '@/components/navigation-bar'
 import { Button } from '@/components/ui/button'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import withAuth, { IWithAuth } from '@/hooks/withAuth'
 import { ChevronRightIcon } from 'lucide-react'
 
@@ -15,9 +16,13 @@ const Pie = dynamic(
   () => import('@ant-design/plots').then(mod => mod.Pie) as any,
   { ssr: false }
 )
+const Column = dynamic(
+  () => import('@ant-design/plots').then(mod => mod.Column) as any,
+  { ssr: false }
+)
 
-const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
-  const config: any = {
+const App: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
+  const configPie: any = {
     data: [
       { type: 'Depress', value: 27 },
       { type: 'Anxiety', value: 25 },
@@ -38,6 +43,62 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
     }
   }
 
+  const dataWeekly: any = {
+    data: [
+      { type: 'Mon', value: 3 },
+      { type: 'Tue', value: 5 },
+      { type: 'Wed', value: 1 },
+      { type: 'Thu', value: 6 },
+      { type: 'Sat', value: 5 },
+      { type: 'Fri', value: 3 },
+      { type: 'Sun', value: 2 }
+    ]
+  }
+
+  const dataMonthly: any = {
+    data: [
+      { type: 'Jan', value: 3 },
+      { type: 'Feb', value: 5 },
+      { type: 'Mar', value: 1 },
+      { type: 'Apr', value: 6 },
+      { type: 'May', value: 5 },
+      { type: 'Jun', value: 3 },
+      { type: 'Jul', value: 6 },
+      { type: 'Aug', value: 8 },
+      { type: 'Sep', value: 5 },
+      { type: 'Oct', value: 9 },
+      { type: 'Nov', value: 2 },
+      { type: 'Dec', value: 7 }
+    ]
+  }
+
+  const configColumn: any = {
+    axis: {
+      x: false,
+      y: false
+    },
+    xField: 'type',
+    yField: 'value',
+    style: {
+      fill: ({ type }) => {
+        if (type === 'Sun') {
+          return '#13C2C2'
+        }
+        return '#ABDCDB'
+      },
+      // radiusTopRight: 6,
+      radius: 6,
+      paddingBottom: 10
+    },
+    label: {
+      position: 'bottom',
+      text: originData => originData.value
+    },
+    legend: false,
+    arrow: false,
+    tooltip: false
+  }
+
   const renderHomeContent = () => {
     switch (userRole) {
       case 'patient':
@@ -49,7 +110,7 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
                   What’s the turbulence on your mind?
                 </div>
                 <div className=''>
-                  <Pie height={180} {...config} />
+                  <Pie height={180} {...configPie} />
                 </div>
                 <div className='text-[10px]'>
                   *based on your data previous record, not necessarily in recent
@@ -95,15 +156,17 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
 
             {/* Popular Assesment */}
             <div className='bg-[#F9F9F9] p-4'>
-              <div className='flex justify-between'>
+              <div className='flex justify-between text-muted'>
                 <span className='mb-2 text-[14px] font-bold'>
                   Popular Assesment
                 </span>
-                <span className='text-[12px]'>See All</span>
+                <Link className='text-[12px]' href={'/'}>
+                  See All
+                </Link>
               </div>
               <div>
-                <ScrollArea className='w-full whitespace-nowrap rounded-md border'>
-                  <div className='flex w-max space-x-4 p-4'>
+                <ScrollArea className='w-full whitespace-nowrap pb-4'>
+                  <div className='flex w-max space-x-4'>
                     {Array(5)
                       .fill(undefined)
                       .map((_, index: number) => (
@@ -141,7 +204,9 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
                 <span className='mb-2 text-[14px] font-bold'>
                   Previous Record Summary
                 </span>
-                <span className='text-[12px]'>See All</span>
+                <Link className='text-[12px]' href={'/'}>
+                  See All
+                </Link>
               </div>
               <Link href={'/'} className='card mt-4 flex flex-col gap-2 p-4'>
                 <div className='flex'>
@@ -202,10 +267,131 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
         )
       case 'clinician':
         return (
-          <div className='p-4'>
-            <p className='text-center'>
-              Halo clinician, ini tampilan khusus untuk clinician.
-            </p>
+          <div className='mt-[-24px] rounded-[16px] bg-white'>
+            <div className='p-4'>
+              <div className='rounded-lg bg-[#F9F9F9] p-[16px]'>
+                <div className='mb-4 flex justify-between'>
+                  <div className='text-[14px] font-bold text-[#2C2F3599]'>
+                    Handled Sessions
+                  </div>
+                  <Link className='text-[12px] text-secondary' href={'/'}>
+                    Generate Report
+                  </Link>
+                </div>
+                <Tabs defaultValue='weekly' className='w-full'>
+                  <TabsList className='grid h-fit w-full grid-cols-2 bg-[#F4F4F4] p-2'>
+                    <TabsTrigger
+                      className='text-muted data-[state=active]:text-[#ABDCDB]'
+                      value='weekly'
+                    >
+                      Weekly
+                    </TabsTrigger>
+                    <TabsTrigger
+                      className='text-muted data-[state=active]:text-[#ABDCDB]'
+                      value='monthly'
+                    >
+                      Monthly
+                    </TabsTrigger>
+                  </TabsList>
+                  <TabsContent value='weekly'>
+                    <Column height={180} {...configColumn} {...dataWeekly} />
+                  </TabsContent>
+                  <TabsContent value='monthly'>
+                    <Column height={180} {...configColumn} {...dataMonthly} />
+                  </TabsContent>
+                </Tabs>
+                <div className='text-[10px]'>
+                  *based on your data previous record, not necessarily in recent
+                  period
+                </div>
+              </div>
+            </div>
+
+            <div className='flex gap-4 p-4'>
+              <Link href={'/'} className='card flex w-full'>
+                <Image
+                  src={'/images/mental-health.svg'}
+                  width={40}
+                  height={40}
+                  alt='writing'
+                />
+                <div className='ml-2 flex flex-col'>
+                  <span className='text-[12px] font-bold text-primary'>
+                    Relax Time
+                  </span>
+                  <span className='text-[10px] text-primary'>
+                    Relax for Better Mental Health
+                  </span>
+                </div>
+              </Link>
+              <Link href={'/'} className='card flex w-full'>
+                <Image
+                  src={'/images/writing.svg'}
+                  width={40}
+                  height={40}
+                  alt='writing'
+                />
+                <div className='ml-2 flex flex-col'>
+                  <span className='text-[12px] font-bold text-primary'>
+                    SOAP Report
+                  </span>
+                  <span className='text-[10px] text-primary'>
+                    Start Writting
+                  </span>
+                </div>
+              </Link>
+            </div>
+
+            <div className='p-4'>
+              <div className='mb-4 text-[14px] font-bold text-muted'>
+                Browse Instruments
+              </div>
+              <div className='grid w-full grid-cols-2 gap-4'>
+                {Array(4)
+                  .fill(undefined)
+                  .map((_, index: number) => (
+                    <Link
+                      key={index}
+                      href={'/'}
+                      className='card flex w-full items-center p-2'
+                    >
+                      <div className='mr-2 h-[40px] w-[40px] rounded-full bg-[#F8F8F8] p-2'>
+                        <div className='max-w[24px]'>
+                          <Image
+                            className='min-h-[24px] min-w-[24px] object-cover'
+                            src='/images/note.svg'
+                            width={24}
+                            height={24}
+                            alt='note'
+                          />
+                        </div>
+                      </div>
+                      <div className='text-[12px]'>
+                        BIG 5 Personality Test lanjut
+                      </div>
+                    </Link>
+                  ))}
+              </div>
+            </div>
+
+            <div className='p-4'>
+              <Link href={'/comunity'} className='card flex'>
+                <Image
+                  src={'/images/mental-health.svg'}
+                  width={40}
+                  height={40}
+                  alt='writing'
+                />
+                <div className='ml-2 flex flex-col'>
+                  <span className='text-[12px] font-bold text-primary'>
+                    Community
+                  </span>
+                  <span className='text-[10px] text-primary'>
+                    Meet with new friend in community
+                  </span>
+                </div>
+              </Link>
+            </div>
           </div>
         )
       default: // guest
@@ -254,10 +440,8 @@ const Home: React.FC<IWithAuth> = ({ userRole, isAuthenticated }) => {
           </NavigationBar>
         </>
       )}
-
-      {/* RENDER CONTENT */}
     </div>
   )
 }
 
-export default withAuth(Home, ['patient', 'clinician'], true)
+export default withAuth(App, ['patient', 'clinician'], true)
