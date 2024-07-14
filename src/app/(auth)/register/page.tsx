c89cebaa3ede9a9f8461d7b10c8c5c89af548055
harynp <harynugrahaputra@gmail.com>
2024-07-15 01:50:01 +0700

    import queryClient from service/api and fix npm run build

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -12,13 +12,12 @@ import {
   validateEmail
 } from '@/utils/validation'
 import { useMutation } from '@tanstack/react-query'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { Suspense, useState } from 'react'
 
-export default function Register() {
+export default function Register({ searchParams }) {
   const router = useRouter()
-  const params = useSearchParams()
-  const userType = params.get('role')
+  const userType = searchParams?.role
 
   const { mutate, isPending } = useMutation<any, unknown, typeof userRegister>({
     mutationFn: (newUser: typeof userRegister) => {
@@ -64,31 +63,41 @@ export default function Register() {
     validateField(field, userRegister[field])
   }
 
-  function validateField(field: any, value: string) {
+  function validateField(field: string, value: string) {
     let newError: string | undefined = undefined
-    if (value == null || value.trim() === '') {
-      if (field === 'retype_password') {
-        newError = `Confirm password is required`
-      } else {
+
+    switch (field) {
+      case 'email':
+        newError = validateEmail(value) ? undefined : 'Invalid email address'
+        break
+      case 'username':
+        if (!value.trim()) {
+          newError = 'Username is required'
+        } else if (value.length < 8) {
+          newError = 'Username must be at least 8 characters long'
+        } else if (!alphaNumeric(value)) {
+          newError = 'Username must contain only letters and numbers'
+        }
+        break
+      case 'password':
+      case 'retype_password':
+        if (!value.trim()) {
+          newError =
+            field === 'retype_password'
+              ? 'Confirm password is required'
+              : 'Password is required'
+        } else if (value.length < 8) {
+          newError = 'Password must be at least 8 characters'
+        } else if (!upperCaseOneCharacter(value)) {
+          newError = 'Password must contain at least one uppercase letter'
+        } else if (!specialCharacter(value)) {
+          newError = 'Password must contain at least one special character'
+        }
+        break
+      default:
         newError = `${capitalizeFirstLetter(field)} is required`
-      }
-    } else if (field === 'email') {
-      newError = validateEmail(value) ? undefined : 'Invalid email address'
-    } else if (field === 'username') {
-      if (value.length < 8) {
-        newError = 'Username must be at least 8 characters long'
-      } else if (!alphaNumeric(value)) {
-        newError = 'Username must contain only letters and numbers'
-      }
-    } else if (field === 'password' || field === 'retype_password') {
-      if (value.length < 8) {
-        newError = 'Password must be at least 8 characters'
-      } else if (!upperCaseOneCharacter(value)) {
-        newError = 'Password must contain at least one uppercase letter'
-      } else if (!specialCharacter(value)) {
-        newError = 'Password must contain at least one special character'
-      }
     }
+
     setErrors(prevErrors => ({ ...prevErrors, [field]: newError }))
   }
 
@@ -133,111 +142,113 @@ export default function Register() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmitRegister}
-      className='flex min-h-screen flex-col items-center justify-between p-4'
-    >
-      <div className='flex flex-grow items-center justify-center'>
-        <LogoKonsulin width={201} height={150} className='object-fit' />
-      </div>
-      <div className='w-full space-y-4'>
-        <p className='text-center text-xl font-semibold capitalize text-secondary'>
-          Daftar Akun
-        </p>
-        <Input
-          id='email'
-          prefixIcon={'/icons/user.png'}
-          placeholder='Masukan Email'
-          name='email'
-          type='email'
-          required
-          onChange={(event: any) =>
-            handleChangeInput('email', event.target.value)
-          }
-          onBlur={() => handleBlur('email')}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
-        />
-        {errors.email && touchedFields.email && (
-          <p className='pl-4 text-[10px] text-red-500'>{errors.email}</p>
-        )}
-        <Input
-          id='username'
-          prefixIcon={'/icons/user.png'}
-          placeholder='Masukan Nama Akun'
-          name='username'
-          type='text'
-          required
-          onChange={(event: any) =>
-            handleChangeInput('username', event.target.value)
-          }
-          onBlur={() => handleBlur('username')}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
-        />
-        {errors.username && touchedFields.username && (
-          <p className='pl-4 text-[10px] text-red-500'>{errors.username}</p>
-        )}
-        <Input
-          prefixIcon={'/icons/lock.png'}
-          suffixIcon={'/icons/eye.png'}
-          placeholder='Masukan Password'
-          name='password'
-          id='password'
-          required
-          type={showPassword.password ? 'text' : 'password'}
-          onChange={(event: any) =>
-            handleChangeInput('password', event.target.value)
-          }
-          onBlur={() => handleBlur('password')}
-          outline={false}
-          className='flex w-full items-center justify-between space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
-          onShow={() => handleShowPassword('password')}
-        />
-        {errors.password && touchedFields.password && (
-          <p className='pl-4 text-[10px] text-red-500'>{errors.password}</p>
-        )}
-        <Input
-          prefixIcon={'/icons/lock.png'}
-          suffixIcon={'/icons/eye.png'}
-          placeholder='Masukan Password'
-          name='retype_password'
-          id='retype_password'
-          required
-          type={showPassword.retype_password ? 'text' : 'password'}
-          onChange={(event: any) =>
-            handleChangeInput('retype_password', event.target.value)
-          }
-          onBlur={() => handleBlur('retype_password')}
-          outline={false}
-          className='flex w-full items-center justify-between space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
-          onShow={() => handleShowPassword('retype_password')}
-        />
-        {errors.retype_password && touchedFields.retype_password && (
-          <p className='pl-4 text-[10px] text-red-500'>
-            {errors.retype_password}
-          </p>
-        )}
-        <div className='pt-8'>
-          <button
-            className='text-md border-1 my-4 w-full rounded-full border-primary bg-secondary p-4 text-white'
-            type='submit'
-            disabled={isPending}
-          >
-            {isPending ? 'Loading...' : 'Daftar Akun'}
-          </button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <form
+        onSubmit={handleSubmitRegister}
+        className='flex min-h-screen flex-col items-center justify-between p-4'
+      >
+        <div className='flex flex-grow items-center justify-center'>
+          <LogoKonsulin width={201} height={150} className='object-fit' />
         </div>
-        <LoginMedia />
-        <p className='w-full pb-12 text-center text-sm'>
-          Sudah punya akun?
-          <span
-            className='cursor-pointer text-[#13C2C2]'
-            onClick={() => router.push('/login')}
-          >
-            &nbsp;Masuk Sekarang
-          </span>
-        </p>
-      </div>
-    </form>
+        <div className='w-full space-y-4'>
+          <p className='text-center text-xl font-semibold capitalize text-secondary'>
+            Daftar Akun
+          </p>
+          <Input
+            id='email'
+            prefixIcon={'/icons/user.png'}
+            placeholder='Masukan Email'
+            name='email'
+            type='email'
+            required
+            onChange={(event: any) =>
+              handleChangeInput('email', event.target.value)
+            }
+            onBlur={() => handleBlur('email')}
+            outline={false}
+            className='flex w-full items-center space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
+          />
+          {errors.email && touchedFields.email && (
+            <p className='pl-4 text-[10px] text-red-500'>{errors.email}</p>
+          )}
+          <Input
+            id='username'
+            prefixIcon={'/icons/user.png'}
+            placeholder='Masukan Nama Akun'
+            name='username'
+            type='text'
+            required
+            onChange={(event: any) =>
+              handleChangeInput('username', event.target.value)
+            }
+            onBlur={() => handleBlur('username')}
+            outline={false}
+            className='flex w-full items-center space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
+          />
+          {errors.username && touchedFields.username && (
+            <p className='pl-4 text-[10px] text-red-500'>{errors.username}</p>
+          )}
+          <Input
+            prefixIcon={'/icons/lock.png'}
+            suffixIcon={'/icons/eye.png'}
+            placeholder='Masukan Password'
+            name='password'
+            id='password'
+            required
+            type={showPassword.password ? 'text' : 'password'}
+            onChange={(event: any) =>
+              handleChangeInput('password', event.target.value)
+            }
+            onBlur={() => handleBlur('password')}
+            outline={false}
+            className='flex w-full items-center justify-between space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
+            onShow={() => handleShowPassword('password')}
+          />
+          {errors.password && touchedFields.password && (
+            <p className='pl-4 text-[10px] text-red-500'>{errors.password}</p>
+          )}
+          <Input
+            prefixIcon={'/icons/lock.png'}
+            suffixIcon={'/icons/eye.png'}
+            placeholder='Masukan Password'
+            name='retype_password'
+            id='retype_password'
+            required
+            type={showPassword.retype_password ? 'text' : 'password'}
+            onChange={(event: any) =>
+              handleChangeInput('retype_password', event.target.value)
+            }
+            onBlur={() => handleBlur('retype_password')}
+            outline={false}
+            className='flex w-full items-center justify-between space-x-[10px] rounded-[16px] border border-[#E3E3E3] p-4'
+            onShow={() => handleShowPassword('retype_password')}
+          />
+          {errors.retype_password && touchedFields.retype_password && (
+            <p className='pl-4 text-[10px] text-red-500'>
+              {errors.retype_password}
+            </p>
+          )}
+          <div className='pt-8'>
+            <button
+              className='text-md border-1 my-4 w-full rounded-full border-primary bg-secondary p-4 text-white'
+              type='submit'
+              disabled={isPending}
+            >
+              {isPending ? 'Loading...' : 'Daftar Akun'}
+            </button>
+          </div>
+          <LoginMedia />
+          <p className='w-full pb-12 text-center text-sm'>
+            Sudah punya akun?
+            <span
+              className='cursor-pointer text-[#13C2C2]'
+              onClick={() => router.push('/login')}
+            >
+              &nbsp;Masuk Sekarang
+            </span>
+          </p>
+        </div>
+      </form>
+    </Suspense>
   )
 }
--- src/components/general/query-provider.tsx
+++ src/components/general/query-provider.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+import { queryClient } from '@/services/api'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 export default function QueryProvider({
   children
