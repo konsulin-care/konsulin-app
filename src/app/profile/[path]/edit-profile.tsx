f391742c7db45928133843c12afbc2d80263e8f2
harynp <harynugrahaputra@gmail.com>
2024-07-17 04:25:07 +0700

    use modal from shadcn for dob calender, fix styling in profile

--- src/app/profile/[path]/edit-profile.tsx
+++ src/app/profile/[path]/edit-profile.tsx
@@ -1,71 +1,108 @@
 import Input from '@/components/general/input'
 import DobCalendar from '@/components/profile/dob-calendar'
-import Dropdown from '@/components/profile/dropdown'
+import DropdownProfile from '@/components/profile/dropdown-profile'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '@/components/ui/drawer'
 import { format } from 'date-fns'
 import Image from 'next/image'
 import { useRef, useState } from 'react'
 
-const educationOptions: any[] = [
+const educationOptions = [
   { value: 'diploma', label: 'Diploma' },
   { value: 'bachelor_degree', label: 'Bachelor Degree' },
   { value: 'master_degree', label: 'Master Degree' },
   { value: 'doctoral_degree', label: 'Doctoral Degree' }
 ]
 
+const genderOptions = [
+  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' }
+]
+
 export default function EditProfile({ userRole }) {
-  const [updateUser, setUpdateUser] = useState<any>({
+  const [updateUser, setUpdateUser] = useState({
     username: '',
     email: '',
     birthdate: undefined,
     whatsapp: '',
     sex: '',
     address: '',
-    education: ['']
+    education: []
   })
 
-  const [userPhoto, setUserPhoto] = useState<any>('/images/sample-foto.svg')
+  const [userPhoto, setUserPhoto] = useState('/images/sample-foto.svg')
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const [dobDrawerOpen, setDobDrawerOpen] = useState(false)
 
-  function handleButtonClick() {
+  const [genderValue, setGenderValue] = useState('')
+  const [educationValue, setEducationValue] = useState<string[]>([])
+
+  const handleGenderSelect = (value: string) => {
+    setGenderValue(value)
+    handleChangeInput('sex', value)
+  }
+
+  const handleEducationSelect = (value: string) => {
+    const selectedOption = educationOptions.find(
+      option => option.value === value
+    )
+    if (selectedOption) {
+      setEducationValue(prevEducation => [
+        ...prevEducation,
+        selectedOption.value
+      ])
+      handleChangeInput('education', [
+        ...updateUser.education,
+        selectedOption.value
+      ])
+    }
+  }
+
+  const handleButtonClick = () => {
     fileInputRef.current?.click()
   }
 
-  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
     if (file) {
       const reader = new FileReader()
       reader.onloadend = () => {
-        setUserPhoto(reader.result)
+        setUserPhoto(reader.result as string)
       }
       reader.readAsDataURL(file)
     }
   }
 
-  function handleChangeInput(label: any, value: any) {
+  const handleChangeInput = (label: string, value: any) => {
     setUpdateUser(prevState => ({
       ...prevState,
       [label]: value
     }))
   }
 
-  function handleAddEducationLevel() {
+  const handleAddEducationLevel = () => {
     setUpdateUser(prevState => ({
       ...prevState,
       education: [...prevState.education, '']
     }))
   }
 
-  function handleEducationChange(index: number, value: string) {
+  const handleEducationChange = (index: number, value: string) => {
     setUpdateUser(prevState => ({
       ...prevState,
-      education: prevState.education.map((edu: any, i: number) =>
+      education: prevState.education.map((edu, i) =>
         i === index ? value : edu
       )
     }))
   }
 
-  function handleEditSave() {
-    updateUser.birthdate = format(updateUser.birthdate, 'yyyy-MM-dd')
+  const handleEditSave = () => {
     const updateUserData = {
       photo: userPhoto,
       ...updateUser
@@ -73,169 +110,180 @@ export default function EditProfile({ userRole }) {
     console.log('Saving updated user profile:', updateUserData)
   }
 
-  function handleDOBChange(value) {
+  const handleDOBChange = (value: any) => {
     setUpdateUser(prevState => ({
       ...prevState,
-      birthdate: value
+      birthdate: format(value, 'yyyy-MM-dd')
     }))
+    setDobDrawerOpen(false)
+  }
+
+  const closeDrawer = () => {
+    setDobDrawerOpen(false)
   }
 
   return (
-    <div className='mt-[-24px] rounded-[16px] bg-white p-4'>
-      <div className='flex flex-col items-center justify-center p-4'>
-        <div className='pb-2'>
-          <Image
-            className='rounded-full'
-            src={userPhoto}
-            width={64}
-            height={64}
-            alt='user-photo'
-          />
-        </div>
-        <div className='flex items-center justify-center space-x-2 px-4 py-2'>
-          <Image
-            src={'/icons/edit-photo.svg'}
-            width={16}
-            height={16}
-            alt='edit-photo'
-          />
-          <span
-            className='cursor-pointer text-xs font-normal text-secondary'
-            onClick={handleButtonClick}
-          >
-            Ganti Photo
-          </span>
-        </div>
-        <input
-          type='file'
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-          accept='image/*'
-        />
-      </div>
-      <div className='flex flex-col space-y-4 py-4'>
-        <Input
-          width={24}
-          height={24}
-          prefixIcon={'/icons/user-edit.png'}
-          placeholder='Masukan Nama Akun'
-          name='username'
-          id='username'
-          type='text'
-          opacity={false}
-          onChange={event => handleChangeInput('username', event.target.value)}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
-        />
-        <Input
-          width={24}
-          height={24}
-          prefixIcon={'/icons/email.svg'}
-          placeholder='Masukan Email'
-          name='email'
-          id='email'
-          type='email'
-          opacity={false}
-          onChange={event => handleChangeInput('email', event.target.value)}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
-        />
-        <DobCalendar value={updateUser.birthdate} onChange={handleDOBChange} />
-        <Input
-          width={24}
-          height={24}
-          prefixIcon={'/icons/region-code.svg'}
-          placeholder='Masukan Nomor Whatsapp'
-          name='whatsapp'
-          id='whatsapp'
-          type='text'
-          opacity={false}
-          onChange={event => handleChangeInput('whatsapp', event.target.value)}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
-        />
-        <Input
-          width={24}
-          height={24}
-          prefixIcon={'/icons/location.svg'}
-          placeholder='Masukan Alamat Tinggal'
-          name='address'
-          id='address'
-          type='text'
-          opacity={false}
-          onChange={event => handleChangeInput('address', event.target.value)}
-          outline={false}
-          className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
-        />
-        {userRole === 'patient' && (
-          <div className='flex justify-between space-x-2'>
-            <Dropdown
-              options={[
-                { value: 'male', label: 'Male' },
-                { value: 'female', label: 'Female' }
-              ]}
-              placeholder='Select Sex'
-              value={updateUser.sex}
-              onChange={value => handleChangeInput('sex', value)}
-              className='flex w-1/2 items-center space-x-[10px] rounded-lg'
+    <>
+      <div className='mt-[-24px] rounded-[16px] bg-white p-4'>
+        <div className='flex flex-col items-center justify-center p-4'>
+          <div className='pb-2'>
+            <Image
+              className='rounded-full'
+              src={userPhoto}
+              width={64}
+              height={64}
+              alt='user-photo'
             />
-
-            {updateUser.education.map((edu: string, index) => (
-              <Dropdown
-                key={index}
-                placeholder='Select Education'
-                options={educationOptions}
-                value={edu}
-                onChange={value => handleEducationChange(index, value)}
-                className='flex w-1/2 items-center space-x-[10px] rounded-lg'
-              />
-            ))}
           </div>
-        )}
-        {userRole === 'clinician' && (
-          <>
-            <Dropdown
-              options={[
-                { value: 'male', label: 'Male' },
-                { value: 'female', label: 'Female' }
-              ]}
-              placeholder='Select Sex'
-              onChange={value => handleChangeInput('sex', value)}
-              className='mt-4 flex w-full items-center space-x-[10px] rounded-lg'
+          <div className='flex items-center justify-center space-x-2 px-4 py-2'>
+            <Image
+              src={'/icons/edit-photo.svg'}
+              width={16}
+              height={16}
+              alt='edit-photo'
             />
-            {updateUser.education.map((edu, index) => (
-              <Dropdown
-                key={index}
-                options={[
-                  { value: 'diploma', label: 'Diploma' },
-                  { value: 'bachelor_degree', label: 'Bachelor Degree' },
-                  { value: 'master_degree', label: 'Master Degree' },
-                  { value: 'doctoral_degree', label: 'Doctoral Degree' }
-                ]}
-                value={edu}
-                placeholder='Select Education'
-                onChange={value => handleEducationChange(index, value)}
-                className='mt-4 flex w-full items-center space-x-[10px] rounded-lg'
-              />
-            ))}
-            <p
-              className='text-center text-sm font-normal'
-              onClick={handleAddEducationLevel}
+            <span
+              className='cursor-pointer text-xs font-normal text-secondary'
+              onClick={handleButtonClick}
             >
-              + Add Education Level
-            </p>
-          </>
-        )}
-        <div className='py-4'></div>
-        <button
-          className='text-md border-1 w-full rounded-full border-primary bg-secondary p-4 font-semibold text-white'
-          type='submit'
-          onClick={handleEditSave}
-        >
-          Simpan
-        </button>
+              Ganti Photo
+            </span>
+          </div>
+          <input
+            type='file'
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+            accept='image/*'
+          />
+        </div>
+        <div className='flex flex-col space-y-4 py-4'>
+          <Input
+            width={24}
+            height={24}
+            prefixIcon={'/icons/user-edit.png'}
+            placeholder='Masukan Nama Akun'
+            name='username'
+            id='username'
+            type='text'
+            opacity={false}
+            onChange={event =>
+              handleChangeInput('username', event.target.value)
+            }
+            outline={false}
+            className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
+          />
+          <div
+            className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
+            onClick={() => setDobDrawerOpen(true)}
+          >
+            <Image
+              src={'/icons/calendar-edit.png'}
+              alt='calendar-icon'
+              width={24}
+              height={24}
+            />
+            <div className='flex flex-grow justify-start text-sm'>
+              {updateUser.birthdate
+                ? format(updateUser.birthdate, 'yyyy-MM-dd')
+                : 'Masukan Tanggal Lahir'}
+            </div>
+          </div>
+          <Input
+            width={24}
+            height={24}
+            prefixIcon={'/icons/region-code.svg'}
+            placeholder='Masukan Nomor Whatsapp'
+            name='whatsapp'
+            id='whatsapp'
+            type='text'
+            opacity={false}
+            onChange={event =>
+              handleChangeInput('whatsapp', event.target.value)
+            }
+            outline={false}
+            className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
+          />
+          <Input
+            width={24}
+            height={24}
+            prefixIcon={'/icons/location.svg'}
+            placeholder='Masukan Alamat Tinggal'
+            name='address'
+            id='address'
+            type='text'
+            opacity={false}
+            onChange={event => handleChangeInput('address', event.target.value)}
+            outline={false}
+            className='flex w-full items-center space-x-[10px] rounded-lg border border-[#E3E3E3] p-4'
+          />
+          {userRole === 'patient' && (
+            <div className='flex w-full flex-grow justify-between space-x-2'>
+              <DropdownProfile
+                options={genderOptions}
+                value={genderValue}
+                onSelect={handleGenderSelect}
+                placeholder='Pilih Gender'
+              />
+
+              <DropdownProfile
+                options={educationOptions}
+                value={educationValue[0] || ''}
+                onSelect={handleEducationSelect}
+                placeholder='Pilih Pendidikan'
+              />
+            </div>
+          )}
+          {userRole === 'clinician' && (
+            <>
+              <DropdownProfile
+                options={genderOptions}
+                value={genderValue}
+                onSelect={handleGenderSelect}
+                placeholder='Pilih Gender'
+              />
+              {updateUser.education.map((edu, index) => (
+                <DropdownProfile
+                  key={`${edu}-${index}`}
+                  options={educationOptions}
+                  value={edu}
+                  onSelect={value => handleEducationChange(index, value)}
+                  placeholder='Pilih Pendidikan'
+                />
+              ))}
+              <p
+                className='text-center text-sm font-normal'
+                onClick={handleAddEducationLevel}
+              >
+                + Add Education Level
+              </p>
+            </>
+          )}
+          <div className='py-4'></div>
+          <button
+            className='text-md border-1 w-full rounded-full border-primary bg-secondary p-4 font-semibold text-white'
+            type='submit'
+            onClick={handleEditSave}
+          >
+            Simpan
+          </button>
+        </div>
       </div>
-    </div>
+      <Drawer open={dobDrawerOpen} onClose={closeDrawer}>
+        <DrawerTrigger asChild>
+          <div />
+        </DrawerTrigger>
+        <DrawerContent className='p-4'>
+          <DrawerHeader>
+            <DrawerTitle></DrawerTitle>
+            <DrawerDescription></DrawerDescription>
+          </DrawerHeader>
+          <DobCalendar
+            value={updateUser.birthdate}
+            onChange={handleDOBChange}
+          />
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
--- src/components/profile/schedule.tsx
+++ src/components/profile/schedule.tsx
@@ -8,27 +8,25 @@ export default function Schedule({ name, time, date }) {
         <p className='text-sm font-bold text-[#2C2F35] opacity-60'>
           Schedule Active
         </p>
-        <Link href='/' className='text-sm text-[#2C2F35] opacity-60'>
+        <Link href='/' className='text-xs text-[#2C2F35] opacity-60'>
           See All
         </Link>
       </div>
-      <div className='flex items-center justify-between'>
-        <div className='p-4'>
-          <Image
-            src={'icons/calendar.svg'}
-            alt='calendar-icons'
-            width={26}
-            height={25}
-          />
-        </div>
-        <div className='flex flex-grow flex-col items-start'>
-          <p className='text-xs text-[#2C2F35] opacity-60'>
+      <div className='flex items-center justify-between p-4'>
+        <Image
+          src={'icons/calendar.svg'}
+          alt='calendar-icons'
+          width={32}
+          height={32}
+        />
+        <div className='flex flex-grow flex-col items-start pl-[10px]'>
+          <p className='pb-1 text-xs text-[#2C2F35] opacity-60'>
             Upcoming Session With
           </p>
           <p className='text-sm font-bold text-[#10958D]'>{name}</p>
         </div>
-        <div className='p-4 text-xs'>
-          <span className='font-bold'>{time}</span> | {date}
+        <div className='pl-5 text-xs'>
+          <span className='font-bold'>{time} | </span> {date}
         </div>
       </div>
     </div>
