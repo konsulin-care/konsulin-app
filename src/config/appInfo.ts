5cd6ceb5acd803d7fb49d3f8129fb22bba655cb2
Yusuf Arifta <yusuf.arifta@elgibor-solution.com>
2025-04-24 00:23:15 +0700

    Add supertokens module

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -1,8 +1,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/references/frontend-sdks/reference#sdk-configuration
   appName: 'Konsulin app',
-  apiDomain: 'https://dev-app.konsulin.care',
-  websiteDomain: 'http://localhost:3000',
-  apiBasePath: '/api/auth',
+  apiDomain: 'https://dev-api.konsulin.care',
+  websiteDomain: 'https://dev-app.konsulin.care',
+  apiBasePath: '/api/v1/auth',
   websiteBasePath: '/auth'
 };
