534ef130ae76211e180c937e652b3c05f7203027
Gilbert Tan <gilbert.tan@satumedis.id>
2025-05-09 17:51:42 +0700

    feat(hooks): add hook for create an appointment

--- src/services/api/appointments.tsx
+++ src/services/api/appointments.tsx
@@ -1,60 +1,37 @@
-import { useMutation, useQuery } from '@tanstack/react-query'
-import { API } from '../api'
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { Bundle } from 'fhir/r4';
+import { API } from '../api';
 
 export const useGetAppointments = () => {
   return useQuery({
     queryKey: ['appointments'],
     queryFn: () => API.get(`/api/v1/appointments`),
     select: response => {
-      return response.data || null
+      return response.data || null;
     }
-  })
-}
+  });
+};
 export const useGetUpcomingAppointments = () => {
   return useQuery({
     queryKey: ['appointments'],
     queryFn: () => API.get(`/api/v1/appointments/upcoming`),
     select: response => {
-      return response.data || null
+      return response.data || null;
     }
-  })
-}
+  });
+};
 
-export interface ICreateAppointmentsPayload {
-  clinician_id: string
-  schedule_id: string
-  date: string
-  time: string
-  session_type: string
-  number_of_sessions: number
-  price_per_session: number
-  problem_brief: string
-}
-
-export const useCreateAppointments = ({
-  clinician_id,
-  schedule_id,
-  date,
-  time,
-  session_type,
-  number_of_sessions,
-  price_per_session,
-  problem_brief
-}: ICreateAppointmentsPayload) => {
+export const useCreateAppointment = () => {
   return useMutation({
     mutationKey: ['create-appointments'],
-    mutationFn: async () => {
-      const response = await API.post('/api/v1/appointments', {
-        clinician_id,
-        schedule_id,
-        date,
-        time,
-        session_type,
-        number_of_sessions,
-        price_per_session,
-        problem_brief
-      })
-      return response.data
+    mutationFn: async (payload: Bundle) => {
+      try {
+        const response = await API.post('/fhir', payload);
+        return response.data;
+      } catch (error) {
+        console.error('Error when booking an appointment:', error);
+        throw error;
+      }
     }
-  })
-}
+  });
+};
