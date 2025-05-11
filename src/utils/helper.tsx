dae244e2426f90be305df19b49fbafc706c7883f
Gilbert Tan <gilbert.tan@satumedis.id>
2025-05-11 20:42:26 +0700

    feat(utils): add helper function to parse appointment response

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -1,13 +1,19 @@
 import { IBundleResponse } from '@/types/record';
 import {
   Annotation,
+  Appointment,
+  AppointmentParticipant,
+  Attachment,
+  Bundle,
+  BundleEntry,
   Coding,
   HumanName,
   Observation,
   Patient,
   Practitioner,
   QuestionnaireItem,
-  QuestionnaireResponse
+  QuestionnaireResponse,
+  Slot
 } from 'fhir/r4';
 
 export const mergeNames = (data: HumanName[]) => {
@@ -143,4 +149,75 @@ export const removeCityPrefix = (input: string): string => {
   if (!input) return '';
 
   return input.replace(/^(Kab\.|Kota)\s+/i, '').trim();
+};
+
+type MergedAppointment = {
+  appointmentId: string;
+  slotStart: string | null;
+  slotEnd: string | null;
+  slotStatus: string | null;
+  appointmentType: string | null;
+  practitionerId: string | null;
+  practitionerName: HumanName[] | null;
+  practitionerPhoto: Attachment[] | null;
+};
+
+export const parseMergedAppointments = (
+  bundle: Bundle
+): MergedAppointment[] => {
+  const appointments = bundle.entry
+    .filter(
+      (entry: BundleEntry) => entry.resource.resourceType === 'Appointment'
+    )
+    .map((entry: BundleEntry) => entry.resource as Appointment);
+
+  const slots = bundle.entry
+    .filter((entry: BundleEntry) => entry.resource.resourceType === 'Slot')
+    .map((entry: BundleEntry) => entry.resource as Slot);
+
+  const practitioners = bundle.entry
+    .filter(
+      (entry: BundleEntry) => entry.resource.resourceType === 'Practitioner'
+    )
+    .map((entry: BundleEntry) => entry.resource as Practitioner);
+
+  const results: MergedAppointment[] = [];
+
+  appointments.forEach((appointment: Appointment) => {
+    // extract slot id
+    const slotReference = appointment.slot && appointment.slot[0]?.reference;
+    const slotId = slotReference ? slotReference.split('/')[1] : null;
+
+    // extract practitioner reference from participants
+    const practitionerParticipant = appointment.participant.find(
+      (participant: AppointmentParticipant) =>
+        participant.actor.reference &&
+        participant.actor.reference.startsWith('Practitioner/')
+    );
+    const practitionerId = practitionerParticipant
+      ? practitionerParticipant.actor.reference.split('/')[1]
+      : null;
+
+    const slotData = slots.find((slot: Slot) => slot.id === slotId);
+    const practitionerData = practitioners.find(
+      (practitioner: Practitioner) => practitioner.id === practitionerId
+    );
+
+    results.push({
+      appointmentId: appointment.id || null,
+      slotStart: slotData?.start || null,
+      slotEnd: slotData?.end || null,
+      slotStatus: slotData?.status || null,
+      appointmentType: appointment.appointmentType?.text || null,
+      practitionerId: practitionerData?.id || null,
+      practitionerName: practitionerData?.name || null,
+      practitionerPhoto: practitionerData?.photo || null
+    });
+  });
+
+  // sort the results by slotStart in ascending order
+  return results.sort((a, b) => {
+    if (!a.slotStart || !b.slotStart) return 0;
+    return new Date(a.slotStart).getTime() - new Date(b.slotStart).getTime();
+  });
 };
