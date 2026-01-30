c1ee725b2f4295aa582693a4657b3cd02ba86d5f
luckyAkbar <luckyakbar1509@gmail.com>
2026-01-30 13:19:57 +0700

    misc: rename constant for anonymous session identifier

--- src/constants/anonymous-session.ts
+++ src/constants/anonymous-session.ts
@@ -1,4 +1,4 @@
 export const ANONYMOUS_SESSION_IDENTIFIER_SYSTEM =
-  'https://konsulin/anonymous-guest';
+  'https://login.konsulin.care/guestid';
 
 export const ANONYMOUS_SESSION_GUEST_ID_STORAGE_KEY = 'konsulin.guest_id';
