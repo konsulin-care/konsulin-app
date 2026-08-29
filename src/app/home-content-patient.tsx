8ac3e777d991f7c4735711ab4d461e87eb46f8a9
lamurian <lamuri.aly8@gmail.com>
2026-08-29 18:41:00 +0200

    fix(home): add gap between recommendation and clinics sections

--- src/app/home-content-patient.tsx
+++ src/app/home-content-patient.tsx
@@ -183,7 +183,7 @@ export default function HomeContentPatient() {
       />
 
       {/* SECONDARY: Quick Actions */}
-      <div className='px-4 pb-4'>
+      <div className='px-4 py-4'>
         <ActionCard
           icon={<Building2 className='h-5 w-5 text-gray-600' />}
           title='Show All Clinics'
